2650f35fdac641d5937d72e8c8be5bda24a39092
domzig <dominikzig@hotmail.co.nz>
2024-03-24 17:50:31 +1100

    Fixed first unit test.

--- apps/chefhat-frontend/specs/index.spec.tsx
+++ apps/chefhat-frontend/specs/index.spec.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Page from '../src/app/page';
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+  })
+) as jest.Mock;
+
 describe('Page', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Page />);
+  it('should render successfully', async () => {
+    const awaitPage = await Page();
+
+    const { baseElement } = render(awaitPage);
+
     expect(baseElement).toBeTruthy();
+    expect(screen.getByText('Chefhat')).toBeTruthy();
   });
 });
